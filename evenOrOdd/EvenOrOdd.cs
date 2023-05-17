f32f41af12ecb71f5c8995ad09d2cd9a5dd731fa
remciszewski <remciszewski@icloud.com>
2023-05-17 21:18:42 +0200

    full project

---
+++ evenOrOdd/EvenOrOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenOrOdd
+{
+    public class EvenOrOdd
+    {
+
+        public static string EOD(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd"; 
+            }
+        }
+    }
+}
---
+++ evenOrOdd/Program.cs
@@ -0,0 +1,17 @@
+﻿using evenOrOdd;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter an integer number to check if it's even or odd: ");
+        string numberString = Console.ReadLine();
+        int number = Convert.ToInt32(numberString);
+
+        Console.Write(EvenOrOdd.EOD(number));
+
+
+    }
+}
